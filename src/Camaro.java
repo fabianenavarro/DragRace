073400fb83ac79a2b513df1ac889525fd0ee2e5d
fabianenavarro <fabianenavarro@yahoo.com>
2023-01-11 09:52:12 -0600

    adding files to git made in the fall of 2019

---
+++ src/Camaro.java
@@ -0,0 +1,10 @@
+public class Camaro extends Car{
+	
+	public Camaro() {
+		
+		setName("2020 Chevy Camaro ZL1");
+		setQMile(11.4);
+		setPlace("2nd");
+		
+	}
+}
---
+++ src/Mustang.java
@@ -0,0 +1,10 @@
+public class Mustang extends Car{
+	
+	public Mustang() {
+		
+		setName("2020 Ford Mustand GT");
+		setQMile(12.2);
+		setPlace("3rd");
+		
+	}
+}
